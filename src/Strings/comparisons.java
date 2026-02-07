ef455d3fe2dea078e31f5a8256b37f83306730b5
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2026-02-07 11:35:38 +0530

    String Comparisions

---
+++ src/Strings/comparisons.java
@@ -0,0 +1,15 @@
+package src.Strings;
+
+public class comparisons {
+    public static void main(String[] args) {
+        String a = "Reyaz";
+        String b = "Reyaz";
+
+        // System.out.println(a==b);
+        String name1 = new String("Reyaz");
+        String name2 = new String("Reyaz");
+        // System.out.println(name1 == name2);
+        System.out.println(name1.equals(name2));
+        System.out.println(name1.charAt(0));
+    }
+}
